4d0f22aa2a718d84eb92e9082291426453d476d2
torresargon25-cmd <torres.argon25@triana.salesianos.edu>
2026-01-23 00:13:08 +0100

    feat: add substract

--- Minicalc.java
+++ Minicalc.java
@@ -1,13 +1,21 @@
 
 Crea el fichero MiniCalc.java con este contenido:
 package ejercicio;
+package ejercicio;
 
-public class miniCalc {
+public class MiniCalc {
 
-	public static void main(String[] args) {
-	
+    public static void main(String[] args) {
+        // Puedes probar aquí si quieres
+        // System.out.println(add(5, 3));
+        // System.out.println(subtract(5, 3));
+    }
 
-		public static int add(int a, int b) { return a + b; }
-	}
+    public static int add(int a, int b) {
+        return a + b;
+    }
 
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
 }
