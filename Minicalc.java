ddc7f2f31d57d5bbd3c1675db5c92eef65555198
torresargon25-cmd <torres.argon25@triana.salesianos.edu>
2026-01-22 23:54:04 +0100

    chore: initial version

---
+++ Minicalc.java
@@ -0,0 +1,13 @@
+
+Crea el fichero MiniCalc.java con este contenido:
+package ejercicio;
+
+public class miniCalc {
+
+	public static void main(String[] args) {
+	
+
+		public static int add(int a, int b) { return a + b; }
+	}
+
+}
